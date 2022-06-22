630be601dd93ee6fa4b0e42a1a261fe7261a850f
Demetra <41930525+s69hub@users.noreply.github.com>
2022-06-22 05:45:14 +0430

    update

--- src/components/Soon/Soon.js
+++ src/components/Soon/Soon.js
@@ -1,67 +1,75 @@
 import React, { Fragment } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import logo from "../../images/logo.svg";
-import dump from "../../images/dump.svg";
+import xusd from "../../images/xusd.svg";
 import Countdown from "react-countdown";
 
 function Soon() {
-
-    const countdownRenderer = ({ days, hours, minutes, seconds, completed }) => {
-      if (completed) {
-        return;
-      } else {
-        return (
-          <Row>
-            <Col md={3} xs={6} className="p-0">
-              <Card className="me-2 card-countdown">
-                <Card.Text>
-                  <h3>{days}</h3>
-                  <small>{days > 1 ? `DAYS` : `DAY`}</small>
-                </Card.Text>
-              </Card>
-            </Col>
-            <Col md={3} xs={6} className="p-0">
-              <Card className="me-2 card-countdown">
-                <Card.Text>
-                  <h3>{hours}</h3>
-                  <small>{hours > 1 ? `HOURS` : `HOUR`}</small>
-                </Card.Text>
-              </Card>
-            </Col>
-            <Col md={3} xs={6} className="p-0 mt-2 mt-md-0">
-              <Card className="me-2 card-countdown">
-                <Card.Text>
-                  <h3>{minutes}</h3>
-                  <small>{minutes > 1 ? `MINUTES` : `MINUTE`}</small>
-                </Card.Text>
-              </Card>
-            </Col>
-            <Col md={3} xs={6} className="p-0 mt-2 mt-md-0">
-              <Card className="me-2 card-countdown">
-                <Card.Text>
-                  <h3>{seconds}</h3>
-                  <small>{seconds > 1 ? `SECONDS` : `SECOND`}</small>
-                </Card.Text>
-              </Card>
-            </Col>
-          </Row>
-        );
-      }
-    };
+  const countdownRenderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      return;
+    } else {
+      return (
+        <Row>
+          <Col md={3} xs={6} className="p-0">
+            <Card className="me-2 card-countdown">
+              <Card.Text>
+                <h3>{days}</h3>
+                <small>{days > 1 ? `DAYS` : `DAY`}</small>
+              </Card.Text>
+            </Card>
+          </Col>
+          <Col md={3} xs={6} className="p-0">
+            <Card className="me-2 card-countdown">
+              <Card.Text>
+                <h3>{hours}</h3>
+                <small>{hours > 1 ? `HOURS` : `HOUR`}</small>
+              </Card.Text>
+            </Card>
+          </Col>
+          <Col md={3} xs={6} className="p-0 mt-2 mt-md-0">
+            <Card className="me-2 card-countdown">
+              <Card.Text>
+                <h3>{minutes}</h3>
+                <small>{minutes > 1 ? `MINUTES` : `MINUTE`}</small>
+              </Card.Text>
+            </Card>
+          </Col>
+          <Col md={3} xs={6} className="p-0 mt-2 mt-md-0">
+            <Card className="me-2 card-countdown">
+              <Card.Text>
+                <h3>{seconds}</h3>
+                <small>{seconds > 1 ? `SECONDS` : `SECOND`}</small>
+              </Card.Text>
+            </Card>
+          </Col>
+        </Row>
+      );
+    }
+  };
 
   return (
     <Container className="pt-0 pt-md-5 text-light">
       <Row>
-        <Col className="d-flex justify-content-center">
+        <Col className="text-center">
           <img src={logo} height="200px" className="logo" />
+          <p>
+            <a
+              href="https://xsurgecrypto.net/"
+              target="_blank"
+              className="link-light"
+            >
+              Powered By <img src={xusd} height={40} />
+            </a>
+          </p>
         </Col>
       </Row>
       <Row>
         <Col lg={12} className="mt-3 text-center">
           <h1 className="soon">DUMP is coming...!</h1>
           <h2 className="fs-6">
-            The first ever fully collatoralized ever-rising token backed by xSurge's
-            Revolutionary Internal Liquidity Pool Technology
+            The first ever fully collateralized ever-rising token backed by
+            xUSD's Revolutionary Internal Liquidity Pool Technology
           </h2>
         </Col>
       </Row>
@@ -69,7 +77,7 @@ function Soon() {
           <p>Launch on { new Date(1657057545000).toLocaleString() } </p>
           </Col> */}
       <Row className="d-flex justify-content-center">
-        <Col xs={8} className="mt-3">
+        <Col xs={8} className="mt-4">
           <Countdown date={1657057545000} renderer={countdownRenderer} />
         </Col>
       </Row>
