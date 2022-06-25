6af9779005149e27a45ab519d8a6b8f9c2c2e1de
Demetra <41930525+s69hub@users.noreply.github.com>
2022-06-25 22:08:39 +0430

    update

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,19 +2,27 @@ import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import Rechart from "../Rechart/Rechart";
 import BagValue from "../BagValue/BagValue";
+import dump from "../../images/dump.svg";
 
 function Info() {
   return (
-    <Container className="pt-0 pt-md-5 text-light">
+    <Container className="text-light">
       <Row>
+        <Col xs={12} className="d-flex justify-content-evenly ">
+          <img
+            src={dump}
+            className="shadow-filter img-fluid info-dump mb-5 mt-0 mt-md-5"
+            alt="DUMP Token!"
+          />
+        </Col>
         <Col md={6}>
-          <Card className="card-transparent me-3 h-100">
+          <Card className="card-transparent me-3 h-100 p-1">
             <Card.Title>Information</Card.Title>
             <BagValue />
           </Card>
         </Col>
         <Col md={6}>
-          <Card className="card-transparent me-3 p-1">
+          <Card className="card-transparent p-1">
             <Card.Title>Chart</Card.Title>
             <Card.Body className="text-dark">
               <Rechart />
